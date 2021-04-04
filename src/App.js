891c20a3f5ce27b63d3ec9315a92566356907341
Layla <laylajohanna@gmail.com>
2021-04-04 12:46:59 +0200

    added redirect from "/" to "/home"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React, { Component } from 'react';
 import Navbar from './components/navigation/Navbar';
 import AppRouter from "./components/shared/routers/AppRouter";
-
+import {Redirect} from 'react-router-dom';
 
 import Home from './components/home/Home';
 import Login from './components/login/Login';
@@ -23,7 +23,10 @@ class App extends Component {
         <Router>
           <Navbar />
           <Switch>
-          <Route path='/home' exact component={Home} />
+            <Route exact path="/" render={() => (
+                <Redirect to="/home"/>
+            )}/>
+            <Route path='/home' exact component={Home} />
           <Route path='/registration' component={Register} />
           <Route path='/login' component={Login} />
           <Route path='/dashboard' component={Dashboard} />
