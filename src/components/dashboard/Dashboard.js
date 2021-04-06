71ca8e56a1e4dc48bfcb9a87a4d8fc7c1bc2dbd2
elBeato <beatus.furrer@gmail.com>
2021-04-06 10:49:11 +0200

    solve merge conflicts

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,37 +5,34 @@ import { api, handleError } from '../../helpers/api';
 import User from '../shared/models/User';
 import { withRouter } from 'react-router-dom';
 import { Button } from '../../views/design/Button';
-import Background from "./Background";
+import GeoAdmin from '../geoAdminMap/GeoAdmin';
 
-//import mountains from '../images/mountains2.png';
+
+import mountains from '../images/mountains2.png';
 
 
 const FormContainer = styled.div`
   margin-top: 2em;
   display: flex;
-  font-size: 30px;
   flex-direction: column;
-  align-items: left;
-  margin-left: 20%;
-  min-height: 200px;
+  align-items: center;
+  min-height: 300px;
   justify-content: center;
-  color: white;
 `;
 
 const Form = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 80%;
-  height: 500px;
+  width: 60%;
+  height: 375px;
   font-size: 16px;
   font-weight: 300;
   padding-left: 37px;
   padding-right: 37px;
-  border-radius: 10px;
-  background: rgb(124, 124, 124, 1);
+  border-radius: 5px;
+  background: linear-gradient(rgb(34, 34, 34, 0.4), rgb(50, 50, 50, 0.4));
   transition: opacity 0.5s ease, transform 0.5s ease;
-  margin-left:10%
 `;
 
 const InputField = styled.input`
@@ -81,7 +78,7 @@ z-index: -1;
 `;
 
 
-//console.log(mountains); // /logo.84287d09.png
+console.log(mountains); // /logo.84287d09.png
 
 
 
@@ -99,25 +96,16 @@ class Dashboard extends React.Component {
     componentDidMount() {}
 
     render() {
-        return <div>
-            <Background></Background>
-            <FormContainer>DASHBOARD</FormContainer>
+        return <div style={{objectFit: 'cover',
+            backgroundImage: `url(${mountains})`,
+            backgroundRepeat: 'no-repeat',
+            width: '100%',
+            height: '100%',
+            position: 'fixed',
+            zIndex: '-1'}}>
+            <center><GeoAdmin /></center>
+            <FormContainer>n</FormContainer>
             <Form>m</Form>
-
-
-{/*             <Users>
-              {this.state.users.map(user => {
-                return (
-                  <PlayerContainer key={user.id}
-                  onClick={() => {
-                    this.openUserProfile(user.id);
-                  }}>
-                    <Player user={user} />
-                  </PlayerContainer>
-                );
-              })}
-            </Users> */}
-
         </div>;
     }
 }
